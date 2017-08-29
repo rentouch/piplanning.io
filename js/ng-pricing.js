83f02ec0104339e43fcb3946c7698dd8692bb49f
Dominique B <domi_burnand@yahoo.de>
2017-08-29 10:47:42 +0200

    [Change] Update landing page (new pricing, video)

--- js/ng-pricing.js
+++ js/ng-pricing.js
@@ -1,38 +1,69 @@
-var app = angular.module("pricingApp", []);
+var app = angular.module("pricingApp", ['angular-click-outside']);
 app.controller("pricingCtrl", function($scope) {
-    cloudPrices = [
-        {size : "5 Teams", price : "$ 4000"},
-        {size : "10 Teams", price : "$ 12,500"},
-        {size : "15 Teams", price : "$ 18,750"},
-        {size : "30 Teams", price : "$ 30,000"},
-        {size : "60 Teams", price : "$ 45,000"},
-        {size : "more Teams", price : "contact us"},
-    ];
     serverPrices = [
-        {size : "Up to 10 teams", price : "$ 8,000"},
-        {size : "15 Teams", price : "$ 16,000"},
-        {size : "20 Teams", price : "$ 22,500"},
-        {size : "30 Teams", price : "$ 28,000"},
-        {size : "45 Teams", price : "$ 35,000"},
-        {size : "60 Teams", price : "$ 40,000"},
-        {size : "90 Teams", price : "$ 50,000"},
+        {size : "Up to 10 teams", price : 8000, teams:10},
+        {size : "15 Teams", price : 16000, teams: 15},
+        {size : "20 Teams", price : 22500, teams: 20},
+        {size : "30 Teams", price : 28000, teams: 30},
+        {size : "45 Teams", price : 35000, teams: 45},
+        {size : "60 Teams", price : 40000, teams: 60},
+        {size : "90 Teams", price : 50000, teams: 90},
         {size : "91 and more", price : "contact us"},
     ];
+
+    $scope.recalc = function(){
+      // CLOUD
+      if ($scope.cloudActive){
+        teamSize = $scope.selectedTeamSize
+        if(teamSize<6){
+          $scope.price = 4000
+          $scope.averagePricePerUser = $scope.price/5/40
+          return
+        }
+        teamPrice = 1250
+        if(teamSize>61){
+          teamPrice = 600
+        }
+        if(teamSize>31){
+          teamPrice = 750
+        }
+        if(teamSize>16){
+          teamPrice = 1000
+        }
+        $scope.price = teamPrice*teamSize
+        $scope.averagePricePerUser = teamPrice/40
+        return
+      }
+      // SERVER
+      if(typeof $scope.selectedPrice.price!="number"){
+        $scope.averagePricePerUser = ""
+        return
+      }
+      $scope.price = $scope.selectedPrice.price
+      $scope.averagePricePerUser = Math.round($scope.selectedPrice.price/$scope.selectedPrice.teams/40)
+    }
 
     $scope.setServer = function(){
         $scope.cloudActive = "";
         $scope.serverActive = "active"
         $scope.prices = serverPrices;
-        $scope.infoLabel = "one-time payment"
         $scope.selectedPrice = $scope.prices[0]
+        $scope.recalc()
     }
 
     $scope.setCloud = function(){
         $scope.cloudActive = "active";
         $scope.serverActive = ""
-        $scope.prices = cloudPrices;
-        $scope.infoLabel = "per year"
-        $scope.selectedPrice = $scope.prices[0]
+        $scope.selectedTeamSize = 10
+        $scope.recalc()
+    }
+
+    $scope.displayInfoBox = false;
+    $scope.showInfo = function(){
+      $scope.displayInfoBox = true;
+    }
+    $scope.hideInfo = function(){
+      $scope.displayInfoBox = false;
     }
 
     $scope.setCloud();
