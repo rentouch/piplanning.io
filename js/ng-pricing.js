d377d9401eafccf73db540604de0722983b66498
Dominique B <domi_burnand@yahoo.de>
2017-09-10 22:02:15 +0200

    [Change] Update pricing

--- js/ng-pricing.js
+++ js/ng-pricing.js
@@ -1,13 +1,13 @@
 var app = angular.module("pricingApp", ['angular-click-outside']);
 app.controller("pricingCtrl", function($scope) {
     serverPrices = [
-        {size : "Up to 10 teams", price : 8000, teams:10},
-        {size : "15 Teams", price : 16000, teams: 15},
-        {size : "20 Teams", price : 22500, teams: 20},
-        {size : "30 Teams", price : 28000, teams: 30},
-        {size : "45 Teams", price : 35000, teams: 45},
-        {size : "60 Teams", price : 40000, teams: 60},
-        {size : "90 Teams", price : 50000, teams: 90},
+        {size : "Up to 10 teams", price : 12000, teams:10},
+        {size : "15 Teams", price : 21000, teams: 15},
+        {size : "20 Teams", price : 28000, teams: 20},
+        {size : "30 Teams", price : 39000, teams: 30},
+        {size : "45 Teams", price : 51750, teams: 45},
+        {size : "60 Teams", price : 60000, teams: 60},
+        {size : "90 Teams", price : 72000, teams: 90},
         {size : "91 and more", price : "contact us"},
     ];
 
@@ -15,29 +15,37 @@ app.controller("pricingCtrl", function($scope) {
     $scope.recalc = function(){
       // CLOUD
       if ($scope.cloudActive){
+        $scope.showContactUs = false;
         teamSize = $scope.selectedTeamSize
+        // 1-5 Teams
         if(teamSize<6){
           $scope.price = 4000
           $scope.averagePricePerUser = $scope.price/5/40
           return
         }
-        teamPrice = 1250
-        $scope.showContactUs = false;
-        if(teamSize>90){
-          $scope.averagePricePerUser = ""
-          $scope.showContactUs = true;
-          return
-        }
-        if(teamSize>61){
-          teamPrice = 600
-        }
-        if(teamSize>31){
+        // 6-15 Teams
+        base = 0
+        rangeBottom = 0
+        teamPrice = 875
+        // 16-30 Teams
+        if(teamSize>15){
+          rangeBottom = 15
+          base = 13125
           teamPrice = 750
         }
-        if(teamSize>16){
-          teamPrice = 1000
+        // 31-60 Teams
+        if(teamSize>30){
+          rangeBottom=30
+          base = 24375
+          teamPrice = 600
         }
-        $scope.price = teamPrice*teamSize
+        // > 60 Teams
+        if(teamSize>60){
+          rangeBottom=60
+          base = 42375
+          teamPrice = 400
+        }
+        $scope.price = base+teamPrice*(teamSize-rangeBottom)
         $scope.averagePricePerUser = teamPrice/40
         return
       }
